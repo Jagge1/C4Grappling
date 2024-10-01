f6c15d8e74658e52659794dde85dd4686c187c69
jacob hellgren <hellgrenjacob@hotmail.com>
2024-10-01 21:19:07 +0200

    Created Header

---
+++ C4Grappling-Website/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>C4 Grappling</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Hem</a></li>
+          <li><a href="#">Om oss</a></li>
+          <li><a href="#">Träning</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
